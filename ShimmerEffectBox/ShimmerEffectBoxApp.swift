7d36c08a668dfbda6864c9fb413b775c9ecaae99
onflyer <salkaorama@gmail.com>
2023-08-26 13:18:01 +0200

    Initial Commit

---
+++ ShimmerEffectBox/ShimmerEffectBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectBoxApp.swift
+//  ShimmerEffectBox
+//
+//  Created by Aleksandar Milidrag on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
